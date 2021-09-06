de059d29d9cd403e9996b622dc52c0f9af4fc055
taufiqdimas11 <taufiqdimas11@gmail.com>
2021-09-06 21:28:53 +0700

    tugas modul 1

---
+++ script.js
@@ -0,0 +1,13 @@
+const jurusan =["teknik", "ekonomi", "budaya", "politik","hukum","kedokteran"];
+const fungsiAsyncAwait = async () => {
+    await mainFunction();
+    await keterangan();
+}
+const keterangan=()=>{
+    console.log("==Universitas Tertutup==");
+}
+const mainFunction= () => {
+    var valueNama = document.getElementById("nama").value; 
+    var valueJurusan = document.getElementById("jurusan").value;
+    (jurusan.indexOf(valueJurusan) > -1)?console.log(valueNama+" terdaftar pada jurusan "+valueJurusan+", silahkan hubungi fakultas terkait"):console.log(valueNama+" tidak terdaftar pada jurusan di kampus ini");
+  }
