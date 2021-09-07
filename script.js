caadaf0b83168d9020fe20a8d0a82d6d58eb9515
septiannugrahaaa <50958784+septiannugrahaaa@users.noreply.github.com>
2021-09-07 10:40:15 +0700

    update tugas mod1
    nambahin tombol clear untuk hapus form sama benerin typo dikit

--- script.js
+++ script.js
@@ -11,3 +11,8 @@ const mainFunction= () => {
     var valueJurusan = document.getElementById("jurusan").value;
     (jurusan.indexOf(valueJurusan) > -1)?console.log(valueNama+" terdaftar pada jurusan "+valueJurusan+", silahkan hubungi fakultas terkait"):console.log(valueNama+" tidak terdaftar pada jurusan di kampus ini");
   }
+
+  function eraseText() {
+    document.getElementById("nama").value = "";
+    document.getElementById("jurusan").value = "";
+   }
